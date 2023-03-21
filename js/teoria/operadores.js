077e6b27c047e5c5ea83581db5c72ef963f242bd
alvaromago <aalvaromago@gmail.com>
2023-03-21 21:08:18 +0100

    Teoría JavaScript

---
+++ js/teoria/operadores.js
@@ -0,0 +1,25 @@
+document.write("<h2> - Operadores - </h2>")
+
+// ARITMÉTICOS
+
+let $num1 = 5;
+document.write("<p>Valor base de mi número: " + $num1 + "</p>")
+// Adición (+)
+$num1 += 3; // 8
+document.write("<p>Adición (+=3): " + $num1 + "</p>");
+// Sustracción
+$num1 -= 3; // 5
+document.write("<p>Sustracción (-=3): " + $num1 + "</p>");
+// Multiplicación
+$num1 *= 2; // 10
+document.write("<p>Multiplicación (*=3): " + $num1 + "</p>");
+// División
+$num1 /= 2; // 5
+document.write("<p>División (/=3): " + $num1 + "</p>");
+// Resto
+$num1 %= 2; // 1
+document.write("<p>Resto (%=3): " + $num1 + "</p>");
+// Exponenciación
+$num1 = 2;
+$num1 **= 2; // 4
+document.write("<p>Exponenciación (**=3): " + $num1 + "</p>");
--- js/teoria/variables.js
+++ js/teoria/variables.js
@@ -6,11 +6,11 @@ let $numero = 14; // type = number
 document.write("<p>El tipo de '" + $numero + "' es: " + typeof($numero) + "</p>");
 let $booleano = true; // type = boolean
 document.write("<p>El tipo de '" + $booleano + "' es: " + typeof($booleano) + "</p>");
-const $pi = 3.141592;
+const $pi = 3.141592; // Creación de una constante
 document.write("<p>Ejemplo de constante pi: " + $pi + "</p>");
 
-document.write("<p>Si concatenas (+) una cadena y un número: " + $cadena + $numero + "</p>");
-document.write("<p>Incluso si contactenas dos variables número: " + $numero + $numero + "</p>");
+document.write("<p>Si concatenas (+) una cadena y un número: " + $cadena + $numero + "</p>"); // Concatenar variable number y string
+document.write("<p>Incluso si contactenas dos variables número: " + $numero + $numero + "</p>"); // Concatenar variables number
 
-let $undefined = 3*"Hola";
+let $undefined = 3*"Hola"; // type = Undefined
 document.write("<p>Multiplicar un número y un string da: " + $undefined + "</p>");
